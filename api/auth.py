d9dcca372f1fc241d0a12a60a90e75c7bbca0d16
Dmitry <dmitry.ryzhenkov@hackaboss.com>
2025-02-14 12:48:08 -0800

    dotenv

--- api/auth.py
+++ api/auth.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 from typing import Annotated, Any
 
@@ -12,8 +13,11 @@ from sqlmodel import select
 
 from database import SessionDep, User
 
-ACCESS_TOKEN_EXP_DAYS = 365
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+from dotenv import load_dotenv
+load_dotenv()
+
+ACCESS_TOKEN_EXP_DAYS = 7
+SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = "HS256"
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
