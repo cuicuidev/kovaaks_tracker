70484827bafc1b12172c299b9ec20a8315b1cdb5
Dmitry <dmitry.ryzhenkov@hackaboss.com>
2025-03-07 10:59:19 -0800

    Benchmarks class fixed

--- streamlit/benchmarks.py
+++ streamlit/benchmarks.py
@@ -19,19 +19,40 @@ class Benchmark:
     def get_energy(self, score, hash_):
         t1, t2, t3, t4 = self.thresholds[hash_]
         e1, e2, e3, e4 = self.energy_thresholds
+        previous_energy = e1 - 100
+        max_energy = e4 + 99
 
-        match_array = [0, t1 - (t2 - t1), t1, t2, t3, t4]
+        if e1 == 100: # Novice
+            match_array = [0, t1, t2, t3, t4]
+        elif e1 == 500: # Intermediate
+            match_array = [0, t1 - (t2 - t1), t1, t2, t3, t4]
+        else: # Advanced
+            match_array = [0, t1 - (t2 - t1), t1, t2, t3, t4]
+
         match_idx = match(score, match_array)
         if match_idx == 0:
             return 0
 
-        base = choose(match_idx, 0, self.energy_thresholds[0] - 100, e1, e2, e3, e4)
-        adjustment = score - choose(match_idx, 0, t1 - (t2 - t1), t1, t2, t3, t4)
-        denominator = choose(match_idx, t1 - (t2 - t1), t2 - t1, t2 - t1, t3 - t2, t4 - t3, t4 - t3)
-        factor = choose(match_idx, self.energy_thresholds[0] - 100, e1 - self.energy_thresholds[0] - 100, e2 - e1, e3 - e2, e4 - e3, e4 - e3)
+        if e1 == 100: # Novice
+            base = choose(match_idx, previous_energy, e1, e2, e3, e4)
+            adjustment = score - choose(match_idx, 0, t1, t2, t3, t4)
+            denominator = choose(match_idx, t1, t2 - t1, t3 - t2, t4 - t3, t4 - t3)
+            factor = choose(match_idx, e1, e2 - e1, e3 - e2, e4 - e3, e4 - e3)
+        elif e1 == 500: # Intermediate
+            base = choose(match_idx, 0, previous_energy, e1, e2, e3, e4)
+            adjustment = score - choose(match_idx, 0, t1 - (t2 - t1), t1, t2, t3, t4)
+            denominator = choose(match_idx, t1 - (t2 - t1), t2 - t1, t2 - t1, t3 - t2, t4 - t3, t4 - t3)
+            factor = choose(match_idx, previous_energy, e1 - previous_energy, e2 - e1, e3 - e2, e4 - e3, e4 - e3)
+        else: # Advanced
+            base = choose(match_idx, 0, previous_energy, e1, e2, e3, e4)
+            adjustment = score - choose(match_idx, 0, t1 - (t2 - t1), t1, t2, t3, t4)
+            denominator = choose(match_idx, t1 - (t2 - t1), t2 - t1, t2 - t1, t3 - t2, t4 - t3, t4 - t3)
+            factor = choose(match_idx, previous_energy, e1 - previous_energy, e2 - e1, e3 - e2, e4 - e3, e4 - e3)
 
         if denominator == 0:
             return 0
-        
+      
         result = base + (adjustment / denominator) * factor
+        result = max_energy if result > max_energy else result
+        # print(f"{score} | {base} + ({adjustment}/{denominator}) * {factor} = {result}")  
         return result
--- streamlit/main.py
+++ streamlit/main.py
@@ -13,7 +13,7 @@ from benchmarks import Benchmark
 
 API_URL = "http://127.0.0.1:8000"# "https://chubby-krystyna-cuicuidev-da9ab1a9.koyeb.app"
 
-BENCHMARK_CATEGORIES = ["Dynamic", "Static", "Linear","Precise", "Reactive", "Control","Speed", "Evasive", "Stability"]
+BENCHMARK_CATEGORIES = ["Dynamic", "Static", "Linear", "Precise", "Reactive", "Control", "Speed", "Evasive", "Stability"]
 
 def main():
 
@@ -58,60 +58,79 @@ def main():
         st.sidebar.write("---")        
 
 def show_season(anchor, season, difficulty):
-
     response = requests.get(f"{API_URL}/entry/me/vt-s{season}-{difficulty}/all", headers={"Authorization" : f"Bearer {st.session_state['access_token']}"})
-    data = response.json()
+    
+    if response.status_code != 200:
+        anchor.warning("Unable to retrieve data.")
+        return
+    
+    try:
+        data = response.json()
+        entries = data["entries"]
+        thresholds = data["thresholds"]
+        energy_thresholds = data["energy_thresholds"]
+        del data
+    except:
+        anchor.warning("Unable to retrieve data.")
+        return
+    
+    if not len(set([entry["hash"] for entry in entries if entry["hash"] in thresholds.keys()])) == len(thresholds.keys()):
+        anchor.warning("Not enough data.")
+        return
+    
+    # Processing
+    benchmark = Benchmark(thresholds=thresholds, energy_thresholds=energy_thresholds)
 
-    if data:
-        df = pd.DataFrame(data["entries"])
-        df.loc[:,"ctime"] = df["ctime"].apply(lambda x: datetime.datetime.fromtimestamp(float(x)/1_000_000_000))
+    df = pd.DataFrame(entries)
+    df.loc[:,"ctime"] = df["ctime"].apply(lambda x: datetime.datetime.fromtimestamp(float(x)/1_000_000_000))
 
-        # Get the daily max score
-        benchmark = Benchmark(thresholds=data["thresholds"], energy_thresholds=data["energy_thresholds"])
-        dfs = []
-        for hash_ in benchmark.thresholds.keys():
-            df_ = df[df["hash"] == hash_]
-            scores_series = df_.set_index(df_["ctime"]).resample("D")["score"].max().apply(lambda x: benchmark.get_energy(x, hash_))
-            scores_series.name = df_["scenario"].dropna().unique()[0]
-            dfs.append(scores_series)
-            
-        df__ = pd.concat(dfs, axis=1).interpolate("linear")
-
-        # Get the cumsum of the daily max score
-        series = []
-        for col_pair in np.reshape(df__.columns, shape=(9,2)):
-            df_bench = df__[col_pair]
-            serie = parse_pair(df_bench)
-            series.append(serie)
-            
-        df_energy_cummax = pd.concat(series, axis=1).dropna()
-        fig_energy_progress = px.line(x=df_energy_cummax.index, y=stats.hmean(df_energy_cummax,axis=1), height=400)
-
-        # All time max score for radar graph
-        max_ = df_energy_cummax.max(axis=0)
-        fig_radar = px.line_polar(theta=BENCHMARK_CATEGORIES, r=max_.values, line_close=True, range_r=[data["energy_thresholds"][0] - 100, data["energy_thresholds"][-1] + 100], )
-        fig_radar.update_layout(polar=dict(bgcolor = "rgba(0.0, 0.0, 0.0, 0.0)"))
-
-        # Synthetic data as a placeholder
-        random_data = np.expm1(np.random.normal(5.398594934535208, 1, 1000))
-        random_data = random_data[np.where(random_data < 1250)]
-
-        # Histogram to compare with the population
-        pdf = stats.gaussian_kde(random_data)
-        fig_histogram = px.line(pdf.pdf(np.linspace(-100,1300,1000))*100)
-        energy = stats.hmean(df_energy_cummax.max(axis=0))
-        trace = go.Scatter(
-            x=[energy, energy],
-            y=[0,.25],
-            mode="lines"
-        )
-        fig_histogram.add_trace(trace)
+    # Get the daily max score
+    dfs = []
+    for hash_ in benchmark.thresholds.keys():
+        df_ = df[df["hash"] == hash_]
+        scores_series = df_.set_index(df_["ctime"]).resample("D")["score"].max().apply(lambda x: benchmark.get_energy(x, hash_))
+        scores_series.name = df_["scenario"].dropna().unique()[0]
+        dfs.append(scores_series)
         
-        # Redering all charts
-        c1, c2 = anchor.columns([3,1])
-        c2.plotly_chart(fig_radar)
-        c1.plotly_chart(fig_histogram)
-        anchor.plotly_chart(fig_energy_progress)
+    df__ = pd.concat(dfs, axis=1).interpolate("linear")
+    del dfs
+
+    # Get the cumsum of the daily max score
+    series = []
+    for col_pair in np.reshape(df__.columns, shape=(9,2)):
+        df_bench = df__[col_pair]
+        serie = parse_pair(df_bench)
+        series.append(serie)
+    del df__
+        
+    df_energy_cummax = pd.concat(series, axis=1).dropna()
+    fig_energy_progress = px.line(x=df_energy_cummax.index, y=stats.hmean(df_energy_cummax,axis=1), height=400)
+
+    # All time max score for radar graph
+    max_ = df_energy_cummax.max(axis=0)
+    fig_radar = px.line_polar(theta=BENCHMARK_CATEGORIES, r=max_.values, line_close=True, range_r=[energy_thresholds[0] - 100, energy_thresholds[-1] + 100], )
+    fig_radar.update_layout(polar=dict(bgcolor = "rgba(0.0, 0.0, 0.0, 0.0)"))
+
+    # Synthetic data as a placeholder
+    random_data = np.expm1(np.random.normal(5.398594934535208, 1, 1000))
+    random_data = random_data[np.where(random_data < 1250)]
+
+    # Histogram to compare with the population
+    pdf = stats.gaussian_kde(random_data)
+    fig_histogram = px.line(pdf.pdf(np.linspace(-100,1300,1000))*100)
+    energy = stats.hmean(df_energy_cummax.max(axis=0))
+    trace = go.Scatter(
+        x=[energy, energy],
+        y=[0,.25],
+        mode="lines"
+    )
+    fig_histogram.add_trace(trace)
+    
+    # Redering all charts
+    c1, c2 = anchor.columns([3,1])
+    c2.plotly_chart(fig_radar)
+    c1.plotly_chart(fig_histogram)
+    anchor.plotly_chart(fig_energy_progress)
 
 def authenticate():
     if not st.session_state["access_token"]:
